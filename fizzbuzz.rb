d4f4cf535135f1b49c47dc56f253f8b2a66052e5
Trevor Hegert <thegert138@gmail.com>
2018-09-30 17:24:09 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,13 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 3 == 0
-    puts "Fizz"
+  if int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
+  elsif int % 3 == 0
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz"
+  else
+    return nil  
   end
 end
